9d7b0d97cf2aa79bbaefdde7ba143d99f656f3e7
pradeepveeramaneni <pradeepraov26@gmail.com>
2024-11-09 17:50:01 -0600

    added global exception handler and appinsight, serilog, azure sql database connection established

--- LSC.OnlineCourse.API/Program.cs
+++ LSC.OnlineCourse.API/Program.cs
@@ -1,57 +1,124 @@
 
+using LSC.OnlineCourse.API.Middlewares;
 using LSC.OnlineCourse.Data;
 using LSC.OnlineCourse.Data.Entities;
 using LSC.OnlineCourse.Service;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Templates;
+using System.Linq.Expressions;
+using System.Net;
 
 namespace LSC.OnlineCourse.API
 {
     public class Program
     {
         public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var configuration=builder.Configuration;
-            // Add services to the container.
+        { // Configure Serilog with the settings
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.Debug()
+            .MinimumLevel.Information()
+            .Enrich.FromLogContext()
+            .CreateBootstrapLogger();
 
-            builder.Services.AddDbContextPool<OnlineCourseDbContext>(options =>
+
+            try {
+
+                var builder = WebApplication.CreateBuilder(args);
+                var configuration = builder.Configuration;
+
+                builder.Services.AddApplicationInsightsTelemetry();
+
+                builder.Host.UseSerilog((context, services, loggerConfiguration) => loggerConfiguration
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .WriteTo.Console(new ExpressionTemplate(
+                    // Include trace and span ids when present.
+                    "[{@t:HH:mm:ss} {@l:u3}{#if @tr is not null} ({substring(@tr,0,4)}:{substring(@sp,0,4)}){#end}] {@m}\n{@x}"))
+                .WriteTo.ApplicationInsights(
+                  services.GetRequiredService<TelemetryConfiguration>(),
+                  TelemetryConverter.Traces));
+
+                Log.Information("Starting the smartcodebypradeepraoveeramaneni API...");
+
+
+                // Add services to the container.
+
+                builder.Services.AddDbContextPool<OnlineCourseDbContext>(options =>
+                {
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DbContext"),
+                        provideroptions => provideroptions.EnableRetryOnFailure()
+                        );
+                    options.EnableSensitiveDataLogging();
+                });
+
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                builder.Services.AddScoped<ICourseCategoryRepository, CourseCategoryRepository>();
+                builder.Services.AddScoped<ICourseCategoryService, CourseCategoryService>();
+
+                builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+                builder.Services.AddScoped<ICourseService, CourseService>();
+
+                builder.Services.AddTransient<RequestBodyLoggingMiddleware>();
+                builder.Services.AddTransient<ResponseBodyLoggingMiddleware>();
+
+                var app = builder.Build();
+
+                // app.UseCors("default");
+                app.UseExceptionHandler(errorApp =>
+                {
+                    errorApp.Run(async context =>
+                    {
+                        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                        var exception = exceptionHandlerPathFeature?.Error;
+
+                        Log.Error(exception, "Unhandled exception occurred. {ExceptionDetails}", exception?.ToString());
+                        Console.WriteLine(exception?.ToString());
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        await context.Response.WriteAsync("An unexpected error occurred. Please try again later.");
+                    });
+                });
+
+                app.UseMiddleware<RequestResponseLoggingMiddleware>();
+                app.UseMiddleware<RequestBodyLoggingMiddleware>();
+                app.UseMiddleware<ResponseBodyLoggingMiddleware>();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+
+                app.MapControllers();
+
+                app.Run();
+            }
+            catch (Exception ex)
             {
-                options.UseSqlServer(
-                    configuration.GetConnectionString("DbContext"),
-                    provideroptions=>provideroptions.EnableRetryOnFailure()
-                    );
-                options.EnableSensitiveDataLogging();
-            });
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddScoped<ICourseCategoryRepository, CourseCategoryRepository>();
-            builder.Services.AddScoped<ICourseCategoryService, CourseCategoryService>();
-
-            builder.Services.AddScoped<ICourseRepository, CourseRepository>();
-            builder.Services.AddScoped<ICourseService, CourseService>();
-
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                Log.CloseAndFlush();
             }
 
-            app.UseHttpsRedirection();
 
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
         }
-    }
+}
 }
