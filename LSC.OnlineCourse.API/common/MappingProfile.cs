1e10932d83db21b3c5776bd66f220a48d6dfc7b2
pradeepveeramaneni <pradeepraov26@gmail.com>
2024-11-21 15:53:25 -0600

    added automapper and blob configuration key

--- LSC.OnlineCourse.API/common/MappingProfile.cs
+++ LSC.OnlineCourse.API/common/MappingProfile.cs
@@ -13,6 +13,25 @@ namespace LSC.OnlineCourse.API.common
 
             CreateMap<VideoRequestModel, VideoRequest>()
                 .ForMember(dest => dest.User, src => src.Ignore());
+
+            CreateMap<CourseEnrollmentModel, Enrollment>();
+            CreateMap<Enrollment, CourseEnrollmentModel>()
+                .ForMember(dest => dest.CoursePaymentModel, opt => opt.MapFrom(src =>src.Payments.OrderByDescending(o => o.PaymentDate).FirstOrDefault()))
+                .ForMember(dest => dest.CourseTitle, opt => opt.MapFrom(src => src.Course.Title));  // Mapping for CourseTitle
+
+
+            CreateMap<CoursePaymentModel, Payment>();
+            CreateMap<Payment, CoursePaymentModel>();
+
+            CreateMap<Review, UserReviewModel>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => $"{src.User.LastName}, {src.User.FirstName}"));
+
+            CreateMap<UserReviewModel, Review>()
+                .ForMember(dest => dest.User, opt => opt.Ignore())
+                .ForMember(dest => dest.Course, opt => opt.Ignore());
+
+            CreateMap<InstructorModel, Instructor>();
+            CreateMap<Instructor, InstructorModel>();
         }
 
     }
